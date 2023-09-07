ed0856c52447f08a82635e2f2d62da775e366a3e
Snoopsie1 <oliverrasoli@gmail.com>
2023-09-07 11:29:15 +0200

    basic modal shit done

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -1,16 +1,36 @@
 import { useEffect, useState } from 'react';
 import { Product } from '../types/product';
 import { getAllProducts } from '../services/product';
-import { Button, Table } from 'antd';
+import { Button, Form, Input, Modal, Table } from 'antd';
 import useCustomerStore from '../store/customer.store';
+import { useForm } from 'antd/es/form/Form';
 
 const Products = () => {
-  const [products, setProducts] = useState<Product[]>([]);
+
+  /* - - - - - GLOBAL STATE - - - - - */
   const customer = useCustomerStore((state) => state.customer);
+  /* - - - - - LOCAL STATE - - - - - */
+  const [isSubmittable, setIsSubmittable] = useState(false);
+  const [products, setProducts] = useState<Product[]>([]);
+  const [isOpen, setIsOpen] = useState(false);
+  const [form] = useForm();
+
+  const values = Form.useWatch([], form);
 
   useEffect(() => {
     getAllProducts().then(products => setProducts(products));
   }, []);
+
+  useEffect(() => {
+    form.validateFields({validateOnly: true}).then(
+      () => {
+        setIsSubmittable(true);
+      },
+      () => {
+        setIsSubmittable(false);
+      },
+    );
+  }, [values]);
 
   const columns = [
     {
@@ -30,10 +50,22 @@ const Products = () => {
     key: product.id.toString(),
   }));
 
-  const addProduct = () => {
+  const showModal = () => {
     if (customer?.roleId === 1) {
-      console.log("Modal burde poppe her");
+      setIsOpen(true);
     } 
+  }
+
+  const cancelModal = () => {
+    if (customer?.roleId === 1) {
+      setIsOpen(false);
+    }
+  }
+
+  const onSubmit = () => {
+    console.log('Received Values: ', values);
+    form.resetFields();
+    setIsOpen(false);
   }
 
 
@@ -41,9 +73,54 @@ const Products = () => {
     <div className='w-full p-5'>
       <div className='flex flex-row justify-between mx-5'>
         <h2 className='text-2xl'>Products</h2>
-        { customer?.id === 1 && <Button className='w-40' onClick={() => addProduct()}>Add Product</Button>}
+        { customer?.id === 1 && <Button className='w-40' onClick={() => showModal()}>Add Product</Button>}
       </div>
         <Table dataSource={productsWithKey} columns={columns}/>
+        {/* Modal */}
+        <Modal
+          title='Add Product'
+          open={isOpen}
+          onCancel={cancelModal}
+          footer={null}
+          >
+            <Form
+            name='basic'
+            form={form}
+            style={{ maxWidth: 600 }}
+            onFinish={onSubmit}
+            >
+              <Form.Item 
+                name='name'
+                label='Name'
+                rules={[{ required: true, message: 'Please enter a name!'}]}
+              >
+                <Input/>
+              </Form.Item>
+              <Form.Item 
+                name='price'
+                label='Price'
+                rules={[
+                  { required: true, 
+                    pattern: /^[0-9]*$/,
+                    message: 'Please enter a valid price (numbers only)!',}
+                ]}
+              >
+                <Input/>
+                </Form.Item>
+                <div className='flex justify-end'>
+                <Form.Item className='m-1'>
+                  <Button onClick={cancelModal} danger>
+                    Cancel
+                  </Button>
+                </Form.Item>
+                <Form.Item className='m-1'>
+                  <Button htmlType='submit' disabled={!isSubmittable}>
+                    Submit
+                  </Button>
+                </Form.Item>
+              </div>
+            </Form>
+        </Modal> 
     </div>
   )
 }
